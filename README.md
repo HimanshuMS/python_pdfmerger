059f374f6556267c16a377c4382fce30626e60c7
HimanshuMS <himanshushah1215@gmail.com>
2021-03-07 17:47:04 +0530

    added document to pdf conversion code

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A simple python code for merging multiple pdf files into single pdf file
 
+## WARNING
+The code is still in beta mode so users may experience unexpected errors, please post the issues so I can resolve them
+
 ### Usage:
 `pdfmerger.py -i "<inputfile_1> <inputfile_2> ... <inputfile_n>" -o <outputfile>` 
 
@@ -10,4 +13,6 @@ Note that the pdf files should be in the same directory as the python file, the 
 ### TODO:
 - [x] Merge multiple pdf files into one
 - [ ] Break one pdf file into multiple
+- [x] Convert document files into pdf
 - [ ] Convert other files into pdf
+- [ ] Convert pdf files into other file format
--- pdfmerger.py
+++ pdfmerger.py
@@ -1,37 +1,70 @@
 from PyPDF2 import PdfFileMerger
-import sys, getopt
+import os,sys, getopt, comtypes.client
 
 merger = PdfFileMerger()
 
 def main(argv):
+    try:
+        opts, _ = getopt.getopt(argv, "hmci:o:", ["merge=", "convert=", "input=", "output="])
+    except:
+        print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
+        sys.exit()
+
+    if argv[0] == ("-m" or "--merge"):
+        pdfmerger(opts)
+
+    if argv[0] == ("-c" or "--convert"):
+        converter(opts)
+
+def pdfmerger(opts):
     inputfile = ''
     outputfile = ''
-    try:
-        opts, _ = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except:
-        print("pdfmerger.py -i \"<inputfile_1> <inputfile_2> ... <inputfile_n>\" -o <outputfile>")
-        sys.exit()
 
     for opt, arg in opts:
         if opt == '-h':
-            print("pdfmerger.py -i \"<inputfile_1> <inputfile_2> ... <inputfile_n>\" -o <outputfile>")
+            print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
             sys.exit()
 
-        elif opt in ("-i", "--ifile"):
+        elif opt in ("-i", "--input"):
             inputfile = arg
             for items in inputfile.split():
                 if items.endswith('.pdf'):
                     merger.append(items)
 
-        elif opt in ("-o", "--ofile"):
+        elif opt in ("-o", "--output"):
             outputfile = arg
             if outputfile.endswith('.pdf'):
                 merger.write(outputfile)
             else:
                 merger.write(f'{outputfile}.pdf')
 
-    print(f'Input files : {inputfile}\n- are merged into : {outputfile}')
+    print('Done')
+    sys.exit()
 
+def converter(opts):
+    inputfile = ''
+    outputfile = ''
+    wdFormatPDF = 17
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
+            sys.exit()
+
+        elif opt in ("-i", "--input"):
+            inputfile = os.path.abspath(arg)
+            word = comtypes.client.CreateObject('Word.Application')
+            for items in inputfile.split():
+                doc = word.Documents.Open(items)
+                if opt in("-o", "--output"):
+                    outputfile = arg
+                    doc.SaveAs(outputfile, wdFormatPDF)
+                    doc.Close()
+                    word.Quit()
+                else:
+                    doc.SaveAs(inputfile.split('.')[0], wdFormatPDF)
+                    doc.Close()
+                    word.Quit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
