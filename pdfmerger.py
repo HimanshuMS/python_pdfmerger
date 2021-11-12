574cd78fbd0e3be852611236ea045706a4728027
HimanshuMS <himanshushah1215@gmail.com>
2021-11-12 23:04:47 +0530

    fixed converter outputfile bug

--- pdfmerger.py
+++ pdfmerger.py
@@ -5,27 +5,36 @@ merger = PdfFileMerger()
 
 def main(argv):
     try:
-        opts, _ = getopt.getopt(argv, "hmci:o:", ["merge=", "convert=", "input=", "output="])
+        opts, _ = getopt.getopt(argv, "hmci:o:", ["help=","merge=", "convert=", "input=", "output="])
     except:
         print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
         sys.exit()
 
-    if argv[0] == ("-m" or "--merge"):
-        pdfmerger(opts)
+    try:
+        if argv[0] == ("-m" or "--merge"):
+            pdfmerger(opts)
 
-    if argv[0] == ("-c" or "--convert"):
-        converter(opts)
+        elif argv[0] == ("-c" or "--convert"):
+            converter(opts)
 
-def pdfmerger(opts):
-    inputfile = ''
-    outputfile = ''
-
-    for opt, arg in opts:
-        if opt == '-h':
+        elif argv[0] == ("-h" or "--help"):
             print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
             sys.exit()
 
-        elif opt in ("-i", "--input"):
+        else:
+            print("-h or --help for help")
+            print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
+            sys.exit()
+    
+    except IndexError:
+        print("-h or --help for help")
+        print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
+        sys.exit()
+
+def pdfmerger(opts):
+    for opt, arg in opts:
+
+        if opt in ("-i", "--input"):
             inputfile = arg
             for items in inputfile.split():
                 if items.endswith('.pdf'):
@@ -37,34 +46,32 @@ def pdfmerger(opts):
                 merger.write(outputfile)
             else:
                 merger.write(f'{outputfile}.pdf')
+        
+        else:
+            merger.write('merged.pdf')
 
     print('Done')
     sys.exit()
 
 def converter(opts):
-    inputfile = ''
-    outputfile = ''
     wdFormatPDF = 17
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print("pdfmerger.py [-m | -c] -i <inputfiles> -o <outputfiles>")
-            sys.exit()
-
-        elif opt in ("-i", "--input"):
-            inputfile = os.path.abspath(arg)
-            word = comtypes.client.CreateObject('Word.Application')
-            for items in inputfile.split():
-                doc = word.Documents.Open(items)
-                if opt in("-o", "--output"):
-                    outputfile = arg
-                    doc.SaveAs(outputfile, wdFormatPDF)
-                    doc.Close()
-                    word.Quit()
-                else:
-                    doc.SaveAs(inputfile.split('.')[0], wdFormatPDF)
-                    doc.Close()
-                    word.Quit()
+    if '-i' in opts[1]:
+        inputFile = os.path.abspath(opts[1][1])
+        word = comtypes.client.CreateObject('Word.Application')
+        for items in inputFile.split():
+            doc = word.Documents.Open(items)
+            try:
+                outputFile = os.path.abspath(opts[2][1])
+                doc.SaveAs(outputFile.split('.')[0], wdFormatPDF)
+                doc.Close()
+                word.Quit()
+            except IndexError:
+                doc.SaveAs(inputFile.split('.')[0], wdFormatPDF)
+                doc.Close()
+                word.Quit()
+    print('Done')
+    sys.exit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
